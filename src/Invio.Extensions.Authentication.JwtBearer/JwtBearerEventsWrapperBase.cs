f310580abd303c724fbd418b7c541bd905366106
Brian Caruso <brian@invioinc.com>
2018-11-13 11:28:48 -0800

    Add XML documentation

--- src/Invio.Extensions.Authentication.JwtBearer/JwtBearerEventsWrapperBase.cs
+++ src/Invio.Extensions.Authentication.JwtBearer/JwtBearerEventsWrapperBase.cs
@@ -5,11 +5,31 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace Invio.Extensions.Authentication.JwtBearer {
 
+    /// <summary>
+    ///   Boilerplate wrapper implementation for <see cref="JwtBearerEvents" />
+    ///   event handler class to enable composition of event handling behavior.
+    /// </summary>
+    /// <remarks>
+    ///   This is abstract with a protected constructor as it should only enable
+    ///   true wrappers to implement their desired functionality without having
+    ///   to provide distracting invocations to the wrapped <see cref="JwtBearerEvents" />.
+    /// </remarks>
     public abstract class JwtBearerEventsWrapperBase : JwtBearerEvents {
 
         private JwtBearerEvents inner { get; }
 
-        public JwtBearerEventsWrapperBase(JwtBearerEvents inner) {
+        /// <summary>
+        ///   Creates a <see cref="JwtBearerEvents" /> implementation which
+        ///   does nothing but call the injected wrapped implementation by default.
+        /// </summary>
+        /// <param name="inner">
+        ///   The base <see cref="JwtBearerEvents" /> that will be invoked after the
+        ///   inheriting wrapper performs its side effects.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   Thrown when <paramref name="inner" /> is null.
+        /// </exception>
+        protected JwtBearerEventsWrapperBase(JwtBearerEvents inner) {
             if (inner == null) {
                 throw new ArgumentNullException(nameof(inner));
             }
@@ -17,18 +37,32 @@ namespace Invio.Extensions.Authentication.JwtBearer {
             this.inner = inner;
         }
 
+        /// <summary>
+        ///   Invoked if exceptions are thrown during request processing.
+        ///   The exceptions will be re-thrown after this event unless suppressed.
+        /// </summary>
         public override Task AuthenticationFailed(AuthenticationFailedContext context) {
             return inner.AuthenticationFailed(context);
         }
 
+        /// <summary>
+        ///   Invoked when a protocol message is first received.
+        /// </summary>
         public override Task MessageReceived(MessageReceivedContext context) {
             return inner.MessageReceived(context);
         }
 
+        /// <summary>
+        ///   Invoked after the security token has passed validation
+        ///   and a ClaimsIdentity has been generated.
+        /// </summary>
         public override Task TokenValidated(TokenValidatedContext context) {
             return inner.TokenValidated(context);
         }
 
+        /// <summary>
+        ///   Invoked before a challenge is sent back to the caller.
+        /// </summary>
         public override Task Challenge(JwtBearerChallengeContext context) {
             return inner.Challenge(context);
         }
--- src/Invio.Extensions.Authentication.JwtBearer/QueryStringJwtBearerEventsWrapper.cs
+++ src/Invio.Extensions.Authentication.JwtBearer/QueryStringJwtBearerEventsWrapper.cs
@@ -8,14 +8,73 @@ using Microsoft.Extensions.Primitives;
 
 namespace Invio.Extensions.Authentication.JwtBearer {
 
-    public sealed class QueryStringJwtBearerEventsWrapper : JwtBearerEventsWrapperBase {
+    /// <summary>
+    ///   Creates a wrapper around an inner <see cref="JwtBearerEvents" /> that fetches
+    ///   the API consumer's bearer token out of the query string if it is present.
+    ///   If the query string parameter is not present, this wrapper does nothing.
+    /// </summary>
+    /// <remarks>
+    ///   <para>
+    ///     This can be used when the application consuming a .NET Core WebAPI needs to
+    ///     enable browser sessioins to download images and/or other binary files directly.
+    ///   </para>
+    ///   <para>
+    ///     If a token is specified in both as a query string parameter and in the
+    ///     'Authorization' header, the query string parameter "wins" and will be used
+    ///     even if it is invalid and the header's token is valid.
+    ///   </para>
+    /// </remarks>
+    public class QueryStringJwtBearerEventsWrapper : JwtBearerEventsWrapperBase {
 
+        /// <summary>
+        ///   This is the default name of the query string parameter that will be
+        ///   sought before the 'Authorization' header is parsed using the 'Bearer'
+        ///   authentication scheme.
+        /// </summary>
         public static String DefaultQueryStringKey { get; } = "bearer";
+
+        /// <summary>
+        ///   This is the actual query string parameter that will be sought in all
+        ///   requests to perform authentication. If found, it's value is used for
+        ///   authentication purposes.
+        /// </summary>
         public string QueryStringKey { get; }
 
+        /// <summary>
+        ///   Wraps an instance of <see cref="JwtBearerEvents" /> with a behavior
+        ///   that checks for a token in the query string with a name of "bearer".
+        /// </summary>
+        /// <param name="inner">
+        ///   A base <see cref="JwtBearerEvents" /> implementation that will gain
+        ///   this additional query string inspection behavior.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   Thrown when <paramref name="inner" /> is null.
+        /// </exception>
         public QueryStringJwtBearerEventsWrapper(JwtBearerEvents inner) :
             this(inner, DefaultQueryStringKey) {}
 
+        /// <summary>
+        ///   Wraps an instance of <see cref="JwtBearerEvents" /> with a behavior
+        ///   checks for a token in the query string with a name specified in the
+        ///   <paramref name="queryStringKey" /> parameter.
+        /// </summary>
+        /// <param name="inner">
+        ///   A base <see cref="JwtBearerEvents" /> implementation that will gain
+        ///   this additional query string inspection behavior.
+        /// </param>
+        /// <param name="queryStringKey">
+        ///   The name of the query string parameter that will be sought from requests
+        ///   in order to extract a token.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   Thrown when <paramref name="inner" /> or
+        ///   <paramref name="queryStringKey" /> is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///   Thrown when <paramref name="queryStringKey" /> is an invalid name
+        ///   for a query string parameter.
+        /// </exception>
         public QueryStringJwtBearerEventsWrapper(JwtBearerEvents inner, string queryStringKey) :
             base(inner) {
 
@@ -31,6 +90,19 @@ namespace Invio.Extensions.Authentication.JwtBearer {
             this.QueryStringKey = queryStringKey;
         }
 
+        /// <summary>
+        ///   Checks the web request for the <see cref="QueryStringKey" /> in
+        ///   the request's query string. If it is found, it fetches the token
+        ///   and sets it on the provided <see cref="MessageReceivedContext" />.
+        /// </summary>
+        /// <param name="context">
+        ///   The <see cref="MessageReceivedContext" /> that provides details
+        ///   on the web request that was made. The query string, among other
+        ///   properties relevant to the request, is stored on this value object.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   Thrown when <paramref name="context" /> is null.
+        /// </exception>
         public override Task MessageReceived(MessageReceivedContext context) {
             if (context == null) {
                 throw new ArgumentNullException(nameof(context));
